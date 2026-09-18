ecf8309f2bf18629a31e312ead2ee8c40a34484f
VishSeran <vishwa.sovis@gmail.com>
2026-09-18 22:22:55 +0530

    llm handler-llms updated

---
+++ configs/configurations.py
@@ -0,0 +1,7 @@
+
+from configs.logger import get_logger
+
+
+logger = get_logger("configurations")
+
+GROQ_MODEL = "llama-3.1-8b-instant"
--- llm_handler/llms.py
+++ llm_handler/llms.py
@@ -1,1 +1,14 @@
 
+from configs.logger import get_logger
+
+
+logger = get_logger("llm-handler")
+
+class LLMHandler:
+    
+    def __init__(self):
+        
+        try:
+            
+        except Exception:
+            
