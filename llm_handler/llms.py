a44543369e11a40f4bec8b5b844acae252301168
VishSeran <vishwa.sovis@gmail.com>
2026-09-18 22:28:37 +0530

    llm handler-llms updated

--- llm_handler/llms.py
+++ llm_handler/llms.py
@@ -1,4 +1,6 @@
+from langchain_groq import ChatGroq
 
+from configs.configurations import BASE_CACHE, GROQ_MODEL
 from configs.logger import get_logger
 
 
@@ -6,9 +8,18 @@ logger = get_logger("llm-handler")
 
 class LLMHandler:
     
-    def __init__(self):
+    def __init__(self, temperature:float = 0.2):
         
         try:
             
+            self.llm = ChatGroq (
+                name=GROQ_MODEL,
+                cache=BASE_CACHE,
+                verbose=True,
+                temperature=temperature
+            )
+            
         except Exception:
+            logger.exception("Error in llm handler initialization")
+            raise
             
