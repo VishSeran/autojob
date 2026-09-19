67eb314ec2c49072b41d7ccd5c5df8e43d985f96
VishSeran <vishwa.sovis@gmail.com>
2026-09-19 09:55:18 +0530

    mcp server - init updated

--- configs/configurations.py
+++ configs/configurations.py
@@ -6,3 +6,4 @@ logger = get_logger("configurations")
 
 GROQ_MODEL = "llama-3.1-8b-instant"
 BASE_CACHE = ((Path.cwd()).resolve() / "cache").resolve()
+BASE_DIR = (Path.cwd()).resolve()
---
+++ mcp_server/server.py
@@ -0,0 +1,19 @@
+
+from configs.logger import get_logger
+
+
+logger = get_logger("mcp-server")
+
+class MCPServer:
+    
+    def __init__(self):
+        
+        try:
+            
+            
+            
+        except Exception:
+            logger.exception("Error in mcp server initialize")
+            raise
+    
+    
