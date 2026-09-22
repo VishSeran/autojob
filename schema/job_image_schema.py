2b6d40e1debe274b32977d2f4185a36d0e0804dd
VishSeran <vishwa.sovis@gmail.com>
2026-09-22 14:12:51 +0530

    job image schema updated

---
+++ schema/job_image_schema.py
@@ -0,0 +1,9 @@
+
+from typing import Optional
+from pydantic import BaseModel
+
+class JobImageDetails:
+    
+    description: Optional[str] = None
+    responsibilities: Optional[str] = None
+    requirments: Optional[str] = None
