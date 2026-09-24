185139a5b1525db7280c1fa0d89e0152f915f132
VishSeran <vishwa.sovis@gmail.com>
2026-09-24 12:26:24 +0530

    query handler agent -init updated

---
+++ agents/query_extractor_agent.py
@@ -0,0 +1,16 @@
+
+from configs.logger import get_logger
+
+
+logger = get_logger("query-handler-agent")
+
+class QueryHandlerAgent:
+    
+    def __init__(self):
+        
+        
+        try:
+            
+        except Exception:
+            logger.exception("Unexpected error in query handler agent initialization")
+            raise
