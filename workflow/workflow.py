154b1ff9dfeabbbd4dc2fce5c1d3cd142ed3399b
VishSeran <vishwa.sovis@gmail.com>
2026-09-24 14:55:04 +0530

    query handler agent - get response updated

--- workflow/workflow.py
+++ workflow/workflow.py
@@ -5,6 +5,7 @@ from langgraph.graph import StateGraph
 
 from agents.query_extractor_agent import QueryHandlerAgent
 from configs.logger import get_logger
+from schema.query_schema import QuerySchema
 from workflow.workflow_state import WorkflowState
 
 logger = get_logger("agent-workflow")
@@ -48,11 +49,20 @@ class AgentWorkflow:
                     "final_response" : final_answer
                 }
                 
-            response = await self.query_handler.get_response(query)
+            response: QuerySchema = await self.query_handler.get_response(query)
             logger.info("query response is fetched")
             
+            keyword = response.keyword
+            location = response.location
+            field = response.field
+            no_of_jobs = response.number_of_jobs
+            
+            
             return {
-                
+                "keyword": keyword,
+                "location": location,
+                "job_field": field,
+                "no_of_jobs": no_of_jobs
             }
                 
 
